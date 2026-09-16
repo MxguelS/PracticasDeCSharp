cfec701161bb7d1a44e97ff2d0a42f93c88986b0
dsMxguel <miguel.sanchez9@utp.ac.pa>
2026-09-16 11:05:44 -0500

    feat(initial commit):Parcial#1

---
+++ Parcial/Parcial/Program.cs
@@ -0,0 +1,62 @@
+﻿
+Console.WriteLine("Ingrese la calificacion del primer parcial");
+double califacion1 = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Ingrese la calificacion del segundo parcial");
+double califiacion2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el porcentaje de asistencia:");
+double porcentaje = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingree el nivel de curso\n" +
+    "1" +
+    "\n2" +
+    "\n3 :");
+int curso = int.Parse(Console.ReadLine());
+
+
+double promedio = (califacion1 + califiacion2) / 2;
+double bonificacion = porcentaje * 0.05;
+
+
+
+String alumno;
+if (promedio < 60 || porcentaje < 70)
+{
+    alumno = "reprobado";
+}
+else
+{
+    alumno = "aprobado";
+}
+
+string candidato;
+if (califacion1 >= 90 && califiacion2 >= 90)
+{
+    candidato = " Candidato por Excelencia";
+}
+else
+{
+    candidato = "No fue candidato";
+}
+string categoria;
+
+double final = promedio + bonificacion;
+
+if (curso == 1) { categoria = "Primera"; }
+else if (curso == 2) { categoria = "segunda"; }
+else { categoria = "tercera"; }
+
+string mencion;
+
+if (final >= 90 && porcentaje > 70) { mencion = "Cuadro de honor"; }
+else if (final >= 80 && porcentaje > 70) { mencion = "Mencion honorifica"; }
+else { mencion = "No tiene mencion honorifica"; }
+
+
+
+Console.WriteLine($"Su calificacion final es: {final}");
+Console.WriteLine($"Estado academico: {alumno}");
+Console.WriteLine($"Mencion Honorifica: {mencion}");
+Console.WriteLine($"Categoria de certificacion: {categoria}");
---
+++ ParcialSwitch/ParcialSwitch/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ Crear un programa en C# para clasificar la etapa de vida de una persona y calcular automáticamente el precio de su entrada al cine. 
+El programa solicitará 1 valor de entrada: la edad de la persona (int). 
+Ejecutará 3 operaciones: calcular el precio base de la entrada según la tarifa asignada por el switch, 
+calcular un descuento especial por edad restando un porcentaje fijo obtenido del proceso, y calcular el costo final con un cargo por servicio incluido.
+Llevará 1 estructura switch para definir la categoría de la entrada y su tarifa base según tramos de edad. Como resultado, entregará 2 valores de salida:
+la etapa de vida detectada y el precio final a pagar por el boleto.
+
+precioBase = tarifaBase
+descuentoEspecial = precioBase * 0.10
+costoFinal = (precioBase - descuentoEspecial) + 2.5 // Con cargo por servicio
+ 
+ */
+
+
+class ParcialSwicth
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la edad de la persona");
+        int edad = int.Parse(Console.ReadLine());
+
+      
+        double tarifabase = 10;
+        double descuentoEspecial;
+        double precioBase = tarifabase;
+
+        string tipo;
+        switch (edad)
+        {
+            case <=10:
+                tipo = "niño";
+                descuentoEspecial = 0.10;
+                break;
+
+            case >= 60:
+                tipo = "adulto mayor";
+                descuentoEspecial = 0.15;
+                break;
+
+            case > 10:
+                tipo = "cliente comun";
+                descuentoEspecial = 0;
+                    break;
+            default:
+                Console.WriteLine("Tipo de dato invalido");
+                return;
+        }
+        precioBase = tarifabase * descuentoEspecial;
+        double costoFinal = (tarifabase - precioBase)+ 2.5; // Con cargo por servicio
+
+        Console.WriteLine($"Edad y tipo : {edad} : {tipo}");
+        Console.WriteLine($"precio a pagar por el boleto: {costoFinal}");
+
+    }
+}
